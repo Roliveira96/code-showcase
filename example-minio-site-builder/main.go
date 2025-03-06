31731e10df60e7562772fc5bf56621b60fdddd81
Ricardo de Oliveira <ricardo.oliveira@dgtallab.com.br>
2025-03-06 14:04:12 -0300

    feat: nesse ponto está sendo convertido para webp e sendo salvo no bucket.

--- example-minio-site-builder/main.go
+++ example-minio-site-builder/main.go
@@ -4,18 +4,25 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"golang.org/x/image/draw"
 	"image"
 	"image/jpeg"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
 
 	"github.com/kolesa-team/go-webp/encoder"
 	"github.com/kolesa-team/go-webp/webp"
+)
+
+const (
+	qualidade = 75
+	tamanho   = 1920
 )
 
 func main() {
@@ -87,22 +94,27 @@ func main() {
 		log.Fatalf("Erro ao decodificar a imagem: %v", err)
 	}
 
+	img = resizeImage(img, tamanho)
+
 	// Converte para WebP usando a função
 	webpBuffer, err := convertImageToWebPBuffer(img)
 	if err != nil {
 		log.Fatalf("Erro ao converter para WebP: %v", err)
 	}
 
-	contentType := "image/webp"
-	objectName = "imagens/teste.webp"
-	// André, a função FPutObject() é que manda a imagem pro MinIO.
+	contentType, err := getContentType(*webpBuffer)
+	if err != nil {
+		panic(err)
+	}
+
+	objectName = getWebPImagePath(objectName)
+
 	_, err = minioClient.PutObject(ctx, bucketName, objectName, webpBuffer, int64(webpBuffer.Len()), minio.PutObjectOptions{ContentType: contentType})
 	if err != nil {
 		log.Fatalf("Erro no upload: %v", err)
 	}
 	fmt.Printf("Arquivo '%s' no bucket '%s'! Sucesso!\n", objectName, bucketName)
 
-	// --- Monta a URL manualmente ---
 	// André, como o bucket é público, a gente MONTA a URL na mão. É sempre esse formato.
 	objectURL := fmt.Sprintf("http://%s/%s/%s", endpoint, bucketName, objectName)
 	fmt.Println("URL da imagem:", objectURL)
@@ -123,7 +135,7 @@ func main() {
 func convertImageToWebPBuffer(img image.Image) (*bytes.Buffer, error) {
 	var imagem bytes.Buffer
 
-	options, err := encoder.NewLossyEncoderOptions(encoder.PresetDefault, 75)
+	options, err := encoder.NewLossyEncoderOptions(encoder.PresetDefault, qualidade)
 	if err != nil {
 		return nil, err
 	}
@@ -133,4 +145,34 @@ func convertImageToWebPBuffer(img image.Image) (*bytes.Buffer, error) {
 	}
 
 	return &imagem, nil
+}
+
+func getWebPImagePath(imagePath string) string {
+	outputName := strings.TrimSuffix(imagePath, filepath.Ext(imagePath)) + ".webp"
+	return outputName
+}
+
+func getContentType(imagem bytes.Buffer) (string, error) {
+
+	contentType := http.DetectContentType(imagem.Bytes())
+	contentType = strings.Split(contentType, ";")[0]
+
+	return contentType, nil
+}
+
+func resizeImage(img image.Image, maxWidth int) image.Image {
+	originalWidth := img.Bounds().Dx()
+	originalHeight := img.Bounds().Dy()
+
+	if originalWidth > maxWidth {
+		newHeight := (originalHeight * maxWidth) / originalWidth
+
+		resizedImg := image.NewRGBA(image.Rect(0, 0, maxWidth, newHeight))
+
+		draw.CatmullRom.Scale(resizedImg, resizedImg.Bounds(), img, img.Bounds(), draw.Over, nil)
+
+		return resizedImg
+	}
+
+	return img
 }
